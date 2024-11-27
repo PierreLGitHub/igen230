8bdd8247041e7cf2bd9ab93d9a7faf642393f51b
PierreLGitHub <pierre.lombard1186@gmail.com>
2024-11-27 10:13:14 -0800

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,27 @@
     #include <Arduino.h>
 
     const int pwmChannel = 0;    // PWM channel (0-15)
-    const int pwmFreq = 5000;    // Frequency in Hz 
+    const int pwmFreq = 30000;    // Frequency in Hz 
     const int pwmResolution = 9; // Resolution in bits (2^9 = 512, values from 0 to 511)
 
     //output pins for the motors
     const int rfPin = 14;
     const int rbPin = 12;
     const int enableR = 26;
+
     const int lfPin = 13;
     const int lbPin = 2;
     const int enableL = 27;
+
     const int base_speed = 300;
-    const int max_speed = 511;
+    const int max_speed = pow(2, pwmResolution) - 1;
 
     int right_speed = 0;
     int left_speed = 0;
 
     const int sensor_history = 100;
-    const int threshold = 2000;
-    const int sensor_on = 2000;
+    const int threshold = 1000;
+    const int sensor_on = 1500;
     const int sensor_off = 0;
 
     //input pins for the IR sensors
@@ -34,7 +36,7 @@
     int sensorWeight[] = {-2, -1, 0, 1, 2};
 
     const int Kp = 5;
-    const int Ki = 0;
+    const int Ki = 0.03;
     const int Kd = 40;
 
     int correction;
@@ -51,24 +53,23 @@
 
     void lineCalc();
     void motorControl();
+    void recordHistory();
     void printSensor();
     void printData();
-    int truncate();
 
     void setup() {
         Serial.begin(115200);
 
         // Configure the PWM channels for right motor forward and backward motion   
-        ledcAttachChannel(enableR, pwmFreq, pwmResolution, pwmChannel); // channel 0
         pinMode(enableR, OUTPUT);
         pinMode(rfPin, OUTPUT);
         pinMode(rbPin, OUTPUT);
+        ledcAttachChannel(enableR, pwmFreq, pwmResolution, pwmChannel); // channel 0
 
-        ledcAttachChannel(enableL, pwmFreq, pwmResolution, pwmChannel + 1); // channel 1
         pinMode(enableL, OUTPUT);
         pinMode(lfPin, OUTPUT);
-        pinMode(lbPin, OUTPUT);        
-
+        pinMode(lbPin, OUTPUT);
+        ledcAttachChannel(enableL, pwmFreq, pwmResolution, pwmChannel + 1); // channel 1
 
         pinMode(analogInPin1, INPUT);
         pinMode(analogInPin2, INPUT);
@@ -76,34 +77,12 @@
         pinMode(analogInPin4, INPUT);
         pinMode(analogInPin5, INPUT);
 
-        sensorAvg = 0;
-        sensorSum = 0;
-
-        for (int i = 0; i <= 4; i++){
-            sensor[i] = analogRead(input[i]);
-            
-            if(sensor[i] >= threshold){
-                sensorAvg += sensorWeight[i] * sensor_on;
-                sensorSum += sensor[i];
-
-            }
-            else {
-                sensorAvg += sensorWeight[i] * sensor_off;
-                sensorSum += sensor[i];
-            }       
-        }
-
-        pp = sensorAvg / sensorSum;
-
-        Serial.print("Intial Correction = ");
-        Serial.println(pp);
 
     }
 
     void loop() {
         lineCalc();
         motorControl();
-
     }
 
     void lineCalc(){
@@ -119,34 +98,35 @@
             Serial.print("  ");
 
             if(sensor[i] >= threshold){
-                sensorAvg += sensorWeight[i] * sensor_on * 100;
-                sensorSum += sensor[i];
+                sensorAvg += sensorWeight[i] * sensor_on;
+                sensorSum += sensor_on;
 
             }
             else {
-                sensorAvg += sensorWeight[i] * sensor_off;
-                sensorSum += sensor[i];
+                sensorSum += sensor_off;
             }
         }
 
         Serial.println(" ");
+        
+        if(sensorSum != 0){
+            p = (sensorAvg / sensorSum);
 
-        p = (sensorAvg / sensorSum);
+        }
+        else{
+            p = 0;
+
+        }
+
         intg += p;
         d = p - pp;
         pp = p;
 
         correction = Kp * p + Ki * intg + Kd * d;
 
-        Serial.print("correction = ");
-        Serial.println(correction);
-
-        new_correction = truncate();
-
-        motorControl();
+        new_correction = constrain(correction, -max_speed, max_speed);
 
         printData();
-        delay(500);
 
     }
 
@@ -154,33 +134,22 @@
         right_speed = base_speed + new_correction;
         left_speed = base_speed - new_correction;
 
+
+
         digitalWrite(rfPin, HIGH);
         digitalWrite(rbPin, LOW);
-        ledcWrite(enableR, right_speed); 
-        Serial.print("Right speed: ");
-        Serial.print(right_speed);
-
         digitalWrite(lfPin, HIGH);
         digitalWrite(lbPin, LOW);
-        ledcWrite(enableL, left_speed);
+        ledcWrite(enableR, abs(right_speed));
+        ledcWrite(enableL, abs(left_speed));
+
+        Serial.print("Right speed: ");
+        Serial.print(abs(right_speed));
+        
         Serial.print("     Left speed: ");
-        Serial.println(left_speed);
+        Serial.println(abs(left_speed));
+        
     }
-
-    int truncate(){
-        if(correction >= max_speed){
-            correction = max_speed;
-
-        }
-        else if(correction <= -max_speed){
-            correction = -max_speed;
-
-        }
-
-        return correction;
-
-    }
-
 
     void printData(){
         Serial.print("p = ");
@@ -192,6 +161,13 @@
         Serial.print("  d = ");
         Serial.println(d);
 
-        Serial.print("correction =  ");
-        Serial.print(correction);
+        Serial.print("sensor average = ");
+        Serial.print(sensorAvg);
+
+        Serial.print("  sensor sum =  ");
+        Serial.print(sensorSum);
+
+        Serial.print("  correction =  ");
+        Serial.println(correction);
+    
     }
